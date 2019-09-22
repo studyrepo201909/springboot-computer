d56df6cc9756591810d74a919406df63046e70fb
studyrepo201909 <gohuhx@163.com>
2019-09-22 15:15:05 +0800

    hongxiang -add HelloWorld controller demo

---
+++ src/main/java/com/thoughtworks/springboot/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.springboot.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+  @GetMapping
+  public ResponseEntity<String> getHelloWorld() {
+    return ResponseEntity.ok("Hello World.");
+  }
+}
---
+++ src/test/java/com/thoughtworks/springboot/controller/HelloWorldControllerTest.java
@@ -0,0 +1,31 @@
+package com.thoughtworks.springboot.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class HelloWorldControllerTest {
+
+  @InjectMocks
+  private HelloWorldController helloWorldController;
+
+  @Test
+  void should_return_200_when_call_get_hello_world_api() {
+    ResponseEntity<String> responseEntity = helloWorldController.getHelloWorld();
+
+    assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+  }
+
+  @Test
+  void should_return_HelloWorld_when_call_get_hello_world_api() {
+    ResponseEntity<String> responseEntity = helloWorldController.getHelloWorld();
+
+    assertEquals("Hello World.", responseEntity.getBody());
+  }
+}
